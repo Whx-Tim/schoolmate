5d6f3270879a4b7f4484eecf82e3d08eb0866717
Timx <598357301@qq.com>
2017-05-10 04:04:39 +0800

    add plugins js

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -28,6 +28,7 @@
 @push('scripts.footer')
 <script src="https://cdn.bootcss.com/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="{{ url('js/plugins/classie.js') }}"></script>
 <script src="{{ url('js/plugins/dropzone.min.js') }}"></script>
 <script src="{{ url('js/plugins/summernote.min.js') }}"></script>
 <script src="{{ url('js/plugins/toastr.min.js') }}"></script>
